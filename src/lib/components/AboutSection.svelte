56b5e39e8e1898d39d424baf115b2cc622ce9435
Emagjby <emagjby@gmail.com>
2026-04-08 21:16:39 +0300

    feat(site): build concise about section

---
+++ src/lib/components/AboutSection.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Section from "$lib/components/Section.svelte";
+	import { siteContent } from "$lib/content/site";
+</script>
+
+<Section
+	id="about"
+	title={siteContent.about.title}
+	description={siteContent.about.body}
+/>
